10b73a3104b58cba78b45597b5201635aa298e0a
Hossein <hossein.aflaki70@gmail.com>
2023-04-05 10:38:20 +0430

    get api and show the data in first page

--- src/app/components/RestaurantCard.tsx
+++ src/app/components/RestaurantCard.tsx
@@ -1,24 +1,29 @@
 import Link from "next/link";
+import { RestaurantCardType } from "../page";
 
-export default function RestaurantCard(){
+interface Props{
+  restaurant: RestaurantCardType
+}
+
+export default function RestaurantCard({restaurant}:Props){
     return(
         <main>
              {/* card */}
              <Link href="/restaurant/milestones-grill">
-              <div className="w-64 h-72  overflow-hidden cursor-pointer border">
+              <div className="w-64 h-72  overflow-hidden cursor-pointer border   m-3">
                 <img
-                  src="https://resizer.otstatic.com/v2/photos/wide-huge/2/31852905.jpg"
+                  src={restaurant.main_image}
                   className="w-full h-36"
                 ></img>
-                <h2 className="p-1 font-bold">Milestones Grill</h2>
+                <h2 className="p-1 font-bold">{restaurant.name}</h2>
                 <div className="flex items-start">
                   <div className="mb-2">*****</div>
                   <p className="ml-2">77 reviews</p>
                 </div>
                 <div className="flex text-reg font-light capitalize">
-                  <h2 className="mr-3">Mexican</h2>
+                  <h2 className="mr-3">{restaurant.cuisine.name}</h2>
                   <p className="mr-3">$$$$</p>
-                  <p>Toronto</p>
+                  <p>{restaurant.location.name}</p>
                 </div>
                 <p className="text-small mt-1 font-bold  ">
                   Book 3 times today
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,50 @@
-import NavBar from "./components/NavBar";
+
 import Header from "./components/Header";
 import RestaurantCard from "./components/RestaurantCard";
+import { PrismaClient,Cuisine,Location, PRICE } from "@prisma/client";
 
-export default function Home() {
+
+export interface RestaurantCardType{
+  id:number;
+  name:string;
+  main_image:string;
+  cuisine:Cuisine,
+  location:Location,
+  price:PRICE
+}
+
+const prisma=new PrismaClient();
+
+const fetchRestaurants =async ():Promise<RestaurantCardType[]>=>{
+  const restaurants =await prisma.restaurant.findMany({
+    select:{
+      id:true,
+      name:true,
+      main_image:true,
+      cuisine:true,
+      location:true,
+      price:true 
+    }
+  });
+  return restaurants;
+}
+
+
+export default async function Home() {
+
+  const restaurants= await fetchRestaurants();
+  console.log(restaurants);
+
   return (
     <main>
       <Header />
-      <div className=" flex px-36 py-10">
-        <RestaurantCard />
+      <div className=" flex px-36 py-10 mt-10 flex-wrap   ">
+        {
+         restaurants.map(restaurants=>(
+          <RestaurantCard restaurant={restaurants}  />
+         ))
+        }
+       
       </div>
     </main>
   );
